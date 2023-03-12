7cfe3124cdfd9ec99bba5aee255516871cf9ba3d
rrkkss <rockks@tutanota.com>
2023-03-12 01:47:58 +0100

    made it actually work

---
+++ js/load_defaults.js
@@ -0,0 +1,11 @@
+// browser.storage.local.set(
+//     {
+//         "https://www.reddit.com" : "https://www.teddit.net"
+//     },
+//     {
+//         "https://www.twitter.com" : "https://www.nitter.net"
+//     },
+//     {
+//         "https://www.youtube.com" : "https://www.yewtu.be"
+//     }    
+// );
--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-const redirects = {};
-redirects["https://reddit.com"] = "https://teddit.net";
-redirects["https://twitter.com"] = "https://nitter.net";
-redirects["https://youtube.com"] = "https://yewtu.be";
+let redirects = {};
+redirects["https://www.reddit.com"] = "https://www.teddit.net";
+redirects["https://www.twitter.com"] = "https://www.nitter.net";
+redirects["https://www.youtube.com"] = "https://www.yewtu.be";
 
 function redirect(windowUrl) {
     const regex = /^https?:\/\/.+\.[a-zA-Z]{2,}/;
-    const match = regex.exec(windowUrl);
-    const replace = redirect[match];
+    const match = regex.exec(windowUrl)[0];
+    const replace = redirects[match];
 
-    window.location = windowUrl.replace(match, replace);
+    if (replace) {
+        window.location = windowUrl.replace(match, replace);
+    }
 }
 
-redirect(window.location);
+redirect(window.location.href);
+
+// browser.storage.local.set(
+//     {
+//         "https://www.reddit.com" : "https://www.teddit.net",
+//         "https://www.twitter.com" : "https://www.nitter.net",
+//         "https://www.youtube.com" : "https://www.yewtu.be"
+//     }    
+// );
